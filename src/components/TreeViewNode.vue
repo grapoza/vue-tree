989fa294eb6078eac74ee576268020c3619f848d
Gregg Rapoza <grapoza@gmail.com>
2018-12-27 23:04:13 -0500

    Change for accessibility and best practices, minor cleanup

--- src/components/TreeViewNode.vue
+++ src/components/TreeViewNode.vue
@@ -24,22 +24,22 @@
               v-if="model.children.length > 0 && model.expandable"
               class="tree-view-node-expander"
               :class="{ 'tree-view-node-expanded': model.state.expanded }"
-              @click="toggle">
+              @click="$_treeViewNode_toggle">
               <i class="tree-view-node-expanded-indicator"></i></button>
       <span v-else class="spacer"></span>
 
-      <!-- Checkbox -->
-      <input :id="'cbx-'+nodeId"
-             class="tree-view-node-checkbox"
-             type="checkbox"
-             v-if="model.checkable"
-             v-model="model.state.checked" />
-      <span v-else class="spacer"></span>
-
-      <!-- Text (or label, if checkable) -->
+      <!-- Checkbox and label -->
       <label v-if="model.checkable"
-             :for="'cbx-'+nodeId"
-             class="tree-view-node-text">{{ model.label }}</label>
+             :for="checkboxId"
+             class="tree-view-node-label">
+        <input :id="checkboxId"
+              class="tree-view-node-checkbox"
+              type="checkbox"
+              v-model="model.state.checked" />
+        {{ model.label }}
+      </label>
+
+      <!-- Text (if not checkable) -->
       <span v-else class="tree-view-node-text">{{ model.label }}</span>
     </div>
 
@@ -47,18 +47,18 @@
     <ul v-show="model.state.expanded" 
         v-if="model.children.length > 0 && model.expandable" 
         class="tree-view-node-children">
-      <tree-view-node v-for="(model, index) in model.children"
+      <TreeViewNode v-for="(model, index) in model.children"
                       :key="index"
                       :model="model"
                       :tree-id="treeId">
-      </tree-view-node>
+      </TreeViewNode>
     </ul>
   </li>
 </template>
 
 <script>
   export default {
-    name: 'tree-view-node',
+    name: 'TreeViewNode',
     props: {
       model: {
         type: Object,
@@ -76,9 +76,12 @@
       nodeId() {
         return this.treeId ? `${this.treeId}-${this.model.id}` : null;
       },
+      checkboxId() {
+        return this.nodeId ? `${this.nodeId}-cbx` : null;
+      }
     },
     methods: {
-      toggle(e) {
+      $_treeViewNode_toggle(e) {
         // TODO All behaviors need to be overridable
         if (this.model.children.length > 0 && !e.target.matches("input[type='checkbox']")) {
           this.model.state.expanded = !this.model.state.expanded;
@@ -88,57 +91,60 @@
   };
 </script>
 
-<style lang="scss" scoped>
-  .tree-view-node {
-    padding-left: 0;
+<style lang="scss">
+  .tree-view {
 
-    &:first-child {
-      margin-top: 0;
-    }
+    .tree-view-node {
+      padding-left: 0;
 
-    .tree-view-node-self {
-      display: flex;
-      align-items: center;
-      line-height: 1.2rem;
+      &:first-child {
+        margin-top: 0;
+      }
 
-      .tree-view-node-expander {
-        padding: 0;
-        background: none;
-        border: none;
+      .tree-view-node-self {
+        display: flex;
+        align-items: center;
+        line-height: 1.2rem;
 
-        i.tree-view-node-expanded-indicator {
-          font-style: normal;
-          
-          &::before {
-            content: '+';
-          }
-        }
-
-        &.tree-view-node-expanded {
+        .tree-view-node-expander {
+          padding: 0;
+          background: none;
+          border: none;
 
           i.tree-view-node-expanded-indicator {
-
+            font-style: normal;
+            
             &::before {
-              content: '-';
+              content: '+';
+            }
+          }
+
+          &.tree-view-node-expanded {
+
+            i.tree-view-node-expanded-indicator {
+
+              &::before {
+                content: '-';
+              }
             }
           }
         }
+
+        .tree-view-node-expander, .tree-view-node-checkbox, .spacer {
+          min-width: 1rem;
+          margin: 0;
+        }
+
+        .tree-view-node-text {
+          margin-left: 1.2rem;
+        }
       }
 
-      .tree-view-node-expander, .tree-view-node-checkbox, .spacer {
-        min-width: 1rem;
-        margin: 0;
+      .tree-view-node-children {
+        margin: 0 0 0 2.2rem;
+        padding: 0;
+        list-style: none;
       }
-
-      .tree-view-node-text {
-        margin-left: .2rem;
-      }
-    }
-
-    .tree-view-node-children {
-      margin: 0 0 0 2.2rem;
-      padding: 0;
-      list-style: none;
     }
   }
 </style>
