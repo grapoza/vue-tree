3f16ce525f357a1b0bcc13da7df698d9a082afd3
Gregg Rapoza <grapoza@gmail.com>
2019-01-03 22:56:17 -0500

    Adds tests

--- tests/unit/TreeView.spec.js
+++ tests/unit/TreeView.spec.js
@@ -1,12 +1,60 @@
 import { expect } from 'chai';
-import { shallowMount } from '@vue/test-utils';
+import { createLocalVue, shallowMount } from '@vue/test-utils';
 import TreeView from '../../src/components/TreeView.vue';
+import { generateNodes } from '../data/node-generator.js';
+
+const localVue = createLocalVue();
+
+const defaultPropsData = { model: [] };
+
+function createWrapper(customPropsData, customAttrs) {
+    return shallowMount(TreeView, {
+      propsData: customPropsData || defaultPropsData,
+      localVue,
+      attrs: customAttrs
+    });
+}
 
 describe('TreeView.vue', () => {
-  it('renders props.msg when passed', () => {
-    const wrapper = shallowMount(TreeView, {
-      propsData: { model: [] },
+
+  let wrapper = null;
+
+  afterEach(() => {
+    wrapper.vm.$destroy();
+    wrapper = null;
+  });
+
+  describe('when on an element with an ID', () => {
+
+    beforeEach(() => {
+      wrapper = createWrapper(null, { id: 'my-id' });
     });
-    expect(wrapper.text()).to.include('');
+
+    it('has a uniqueId of the root element ID', () => {
+      expect(wrapper.vm.uniqueId).to.equal(wrapper.attributes('id'));
+    });
+  });
+
+  describe('when on an element without an ID', () => {
+
+    beforeEach(() => {
+      wrapper = createWrapper();
+    });
+
+    it('has a null uniqueId', () => {
+      expect(wrapper.vm.uniqueId).to.be.null;
+    });
+  });
+
+  describe('when getChecked() is called', () => {
+
+    beforeEach(() => {
+      wrapper = createWrapper({ model: generateNodes(['ecs', 'eCs', ['eCs', 'ecs']]) });
+    });
+
+    it('should return checked nodes', () => {
+      let nodes = wrapper.vm.getChecked();
+      expect(nodes.length).to.equal(2);
+    });
   });
 });
