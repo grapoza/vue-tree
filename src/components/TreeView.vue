8ac0cfc1820e4d46fbdfcd79860b582c1d7de7fb
Gregg Rapoza <grapoza@gmail.com>
2019-01-22 12:57:18 -0500

    Allow the id-as-number issue for tree view nodes.

--- src/components/TreeView.vue
+++ src/components/TreeView.vue
@@ -40,8 +40,7 @@
       };
     },
     mounted() {
-      // Force the uniqueId to be a string if present
-      this.$set(this, 'uniqueId', this.$el.id ? this.$el.id + '' : null);
+      this.$set(this, 'uniqueId', this.$el.id ? this.$el.id : null);
     },
     methods: {
       getCheckedCheckboxes() {
