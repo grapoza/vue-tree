abbcd64498d051eaa59589653202a704ca5a554d
Gregg Rapoza (via AppVeyor) <grapoza+appveyor@gmail.com>
2019-10-12 00:57:42 +0000

    AppVeyor commit of docs for version 1.1.0

---
+++ 1.1.0/demo/customStyles.js
@@ -0,0 +1,40 @@
+let defaultData = [
+    {
+        id: 'rootNode',
+        label: 'Root Node',
+        children: [
+            {
+                id: 'subNode',
+                label: 'Subnode'
+            }
+        ]
+    }
+];
+
+let classbasedData = [
+    {
+        id: 'classNode',
+        label: 'Root Node',
+        children: [
+            {
+                id: 'subNode',
+                label: 'Subnode'
+            }
+        ]
+    }
+];
+
+let grayData = [
+    {
+        id: 'grayNode',
+        label: 'Root Node',
+        children: [
+            {
+                id: 'subNode',
+                label: 'Subnode'
+            }
+        ]
+    }
+];
+
+export { defaultData, classbasedData, grayData };
