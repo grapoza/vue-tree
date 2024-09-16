046c15453a47d260ae2cae419921575cee51e646
Gregg Rapoza <grapoza@gmail.com>
2024-09-15 22:22:06 -0400

    Removed old enum files

--- src/enums/selectionMode.js
+++
@@ -1,8 +0,0 @@
-const selectionMode = Object.freeze({
-    None: null,
-    Single: 'single',
-    Multiple: 'multiple',
-    SelectionFollowsFocus: 'selectionFollowsFocus'
-});
-
-export default selectionMode;
