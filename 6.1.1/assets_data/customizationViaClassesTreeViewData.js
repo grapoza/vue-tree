67b70897597b157779eac251affdcadffc9445fe
Gregg Rapoza (via AppVeyor) <grapoza+appveyor@gmail.com>
2024-10-20 16:36:34 +0000

    AppVeyor commit of docs for version 6.1.1

---
+++ 6.1.1/assets_data/customizationViaClassesTreeViewData.js
@@ -0,0 +1,39 @@
+export const treeData = [
+  {
+    id: "customization-class-rootNode",
+    label: "Root Node",
+    children: [
+      {
+        id: "customization-class-subNode",
+        label: "Subnode",
+      },
+    ],
+  },
+];
+
+export function modelDefaults(node) {
+  const defaults = {
+    addChildCallback,
+    customizations: {
+      classes: {
+        treeViewNodeSelf: "large-line",
+        treeViewNodeSelfText: "big-text",
+      },
+    },
+  };
+
+  if (node.id !== "customization-class-rootNode") {
+    defaults.deletable = true;
+  }
+
+  return defaults;
+}
+
+let classChildCounter = 0;
+async function addChildCallback(parentModel) {
+  classChildCounter++;
+  return Promise.resolve({
+    id: `customization-class-child-node${classChildCounter}`,
+    label: `Added Child ${classChildCounter} of ${parentModel.data.id}`,
+  });
+}
